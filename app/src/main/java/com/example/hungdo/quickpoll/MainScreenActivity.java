010eeed61ca64ab2b8bbc063d5a52772951c94b8
hdo33 <hdo33@gatech.edu>
2016-10-22 03:59:39 -0400

    finished menu bar

--- app/src/main/java/com/example/hungdo/quickpoll/MainScreenActivity.java
+++ app/src/main/java/com/example/hungdo/quickpoll/MainScreenActivity.java
@@ -3,6 +3,8 @@ package com.example.hungdo.quickpoll;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainScreenActivity extends AppCompatActivity {
@@ -15,5 +17,22 @@ public class MainScreenActivity extends AppCompatActivity {
 
     public void pollClick(View view) {
         startActivity(new Intent(MainScreenActivity.this, Profile.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_myQuestions) {
+            startActivity(new Intent(MainScreenActivity.this, Profile.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

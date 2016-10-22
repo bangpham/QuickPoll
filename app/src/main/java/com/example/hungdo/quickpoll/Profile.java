80edad78f5ba871b03c44e87c65685be741f8461
Bang <bangpham@gatech.edu>
2016-10-22 07:04:26 -0400

    implement recycler card view aka. feed list.

--- app/src/main/java/com/example/hungdo/quickpoll/Profile.java
+++ app/src/main/java/com/example/hungdo/quickpoll/Profile.java
@@ -1,13 +1,30 @@
 package com.example.hungdo.quickpoll;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class Profile extends AppCompatActivity {
-
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        RecyclerAdapter adapter = new RecyclerAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        //Layout manager for Recycler view
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    public void goToMain() {
+        startActivity(new Intent(Profile.this, MainScreenActivity.class));
     }
 }

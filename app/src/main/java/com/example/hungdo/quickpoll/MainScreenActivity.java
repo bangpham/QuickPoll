57209e7d89b6cb3f058ae4eb5c6e02ad4980733d
hdo33 <hdo33@gatech.edu>
2016-10-22 03:38:22 -0400

    add function button

--- app/src/main/java/com/example/hungdo/quickpoll/MainScreenActivity.java
+++ app/src/main/java/com/example/hungdo/quickpoll/MainScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.hungdo.quickpoll;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+    }
+
+    public void pollClick(View view) {
+        startActivity(new Intent(MainScreenActivity.this, Profile.class));
     }
 }

e04376482a6d40e40bf6d84b485d2d74f6398492
hdo33 <hdo33@gatech.edu>
2016-10-23 00:55:27 -0400

    sa

--- app/src/main/java/model/Facade.java
+++ app/src/main/java/model/Facade.java
@@ -1,5 +1,9 @@
 package model;
 
+import com.example.hungdo.quickpoll.Question;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,7 +12,55 @@ import java.util.List;
 
 public class Facade {
     private static Facade facade;
-    private List<User>
+    private List<User> listUser;
 
-    public static Facade
+    public static void initialize() {
+
+        // initialize the one facade instance for the application
+        facade = new Facade();
+        Question q1 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q2 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q3 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q4 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q5 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q6 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q7 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        List<Question> listQ1 = new ArrayList<>(Arrays.asList(q1, q2, q3, q4, q5, q6, q7));
+        User user1 = new User(4043458278, listQ1);
+        Question q8 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q9 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q10 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q11 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        List<Question> listQ2 = new ArrayList<>(Arrays.asList(q8, q9, q10, q11));
+        User user2 = new User(6783458278, listQ2);
+        Question q12 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q13 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q14 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q15 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q16 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q17 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q18 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q19 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q20 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q21 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q22 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q23 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q24 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        Question q25 = new Question("Who will be the President of the US", "Donald Trum", "Hillary Clinton");
+        List<Question> listQ3 = new ArrayList<>(Arrays.asList(q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25));
+        User user3 = new User(4043453278, listQ3);
+    }
+
+    public Facade() {
+
+        listUser = new ArrayList<>();
+    }
+
+    public static Facade getFacade() {
+        return facade;
+    }
+
+    public static List<User> getListUser() {
+        return listUser;
+    }
 }
